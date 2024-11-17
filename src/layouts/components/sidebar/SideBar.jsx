80d61e5cd0db8b0534adcfb02b9002ad656d940a
Ngan Tran <78363797+ngantrandev@users.noreply.github.com>
2024-11-17 22:45:58 +0700

    update sidebar

--- src/layouts/components/sidebar/SideBar.jsx
+++ src/layouts/components/sidebar/SideBar.jsx
@@ -1,38 +1,56 @@
 import { PropTypes } from 'prop-types';
+import { useCallback, useState } from 'react';
 
 import Menu from '../menu';
 import SideBarItem from './SideBarItem';
 import configs from '../../../configs';
 
+const items = [
+    {
+        title: 'Danh sách dịch vụ',
+        to: configs.routes.admin.dashboard.services,
+    },
+    {
+        title: 'Danh sách khách hàng',
+        to: configs.routes.admin.dashboard.users,
+    },
+    {
+        title: 'Danh sách nhân viên',
+        to: configs.routes.admin.dashboard.staffs,
+    },
+    {
+        title: 'Danh sách đặt lịch',
+        to: configs.routes.admin.dashboard.bookings,
+    },
+    {
+        title: 'Danh sách trạm dịch vụ',
+        to: configs.routes.admin.dashboard.stations,
+    },
+    {
+        title: 'Thống kê',
+        to: configs.routes.admin.statistics,
+    },
+];
+
 function SideBar({ className }) {
+    const [activeItem, setActiveItem] = useState(0);
+
+    const handleChangeActiveItem = useCallback((index) => {
+        setActiveItem(index);
+    }, []);
+
     return (
         <aside className={className}>
             <Menu className='grid w-full grid-cols-3 flex-col gap-y-1 md:flex'>
-                <SideBarItem
-                    title='Danh sách dịch vụ'
-                    to={configs.routes.admin.dashboard.services}
-                />
-                <SideBarItem
-                    title='Danh sách khách hàng'
-                    to={configs.routes.admin.dashboard.users}
-                />
-                <SideBarItem
-                    title='Danh sách nhân viên'
-                    to={configs.routes.admin.dashboard.staffs}
-                />
-                <SideBarItem
-                    title='Danh sách đặt lịch'
-                    to={configs.routes.admin.dashboard.bookings}
-                />
-                <SideBarItem
-                    title='Danh sách trạm dịch vụ'
-                    to={configs.routes.admin.dashboard.stations}
-                />
-
-                <SideBarItem
-                    title='Thống kê'
-                    to={configs.routes.admin.statistics}
-                />
+                {items.map((item, index) => (
+                    <SideBarItem
+                        key={index}
+                        title={item.title}
+                        to={item.to}
+                        active={activeItem === index}
+                        onClick={() => handleChangeActiveItem(index)}
+                    />
+                ))}
             </Menu>
         </aside>
     );
--- src/layouts/components/sidebar/SideBarItem.jsx
+++ src/layouts/components/sidebar/SideBarItem.jsx
@@ -3,13 +3,12 @@ import { PropTypes } from 'prop-types';
 
 const activeStyle = 'bg-primary-light    ';
 
-function SideBarItem({ title, to, icon }) {
+function SideBarItem({ title, to, icon, active, onClick }) {
     return (
         <NavLink
             to={to}
-            className={(nav) =>
-                `block border-2 px-4 py-2 hover:bg-primary-supper-light md:border-0 ${nav.isActive && activeStyle}`
-            }
+            className={`block border-2 px-4 py-2 hover:bg-primary-supper-light md:border-0 ${active && activeStyle}`}
+            onClick={onClick}
         >
             <span>{icon}</span>
             <span className='text-sm md:text-lg'>{title}</span>
@@ -21,6 +20,8 @@ SideBarItem.propTypes = {
     title: PropTypes.string.isRequired,
     to: PropTypes.string.isRequired,
     icon: PropTypes.node,
+    active: PropTypes.bool,
+    onClick: PropTypes.func,
 };
 
 export default SideBarItem;
